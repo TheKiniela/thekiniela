a3904cf328beb17e17b8cd7441b2e3c605f2efd7
Iván Herranz <ihcuesta@gmail.com>
2020-02-21 22:53:12 +0100

    Create game correctly: done

--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,83 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const games = require("../models/game");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index', {user: req.user});
+  res.render('index', {
+    user: req.user
+  });
+ 
 });
 
-// Send info to create new movie
+
+
+// Send info to create new game/bets
 router.post('/', (req, res, next) => {
-  console.log(req.body)
+  let userId = req.user._id
   
-  // games.findOne({ round })
-  //   .then(game => {
-  //     if (game !== null) {
-  //       res.render('/', { message: 'The game already exists' });
-  //       return;
-  //     }
-  //   }
-  // )
+  function saveUsersBets(user) {
+    console.log(user + " lo imprime. VIVA!!")
+  }
+      console.log(req.body)
 
-  const {
-    round,
-    match1,
-    match2,
-    match3,
-    match4,
-    match5,
-    match6,
-    match7,
-    match8,
-    match9,
-    match10,
-    results,
-    users
-  } = req.body;
+      const {
+        round,
+        match1,
+        match2,
+        match3,
+        match4,
+        match5,
+        match6,
+        match7,
+        match8,
+        match9,
+        match10,
+        results,
+        users
+      } = req.body;
 
-  
+      games.findOne({
+          'round': req.body.round
+        })
+        .then(game => {
+          console.log(req.body.round)
+          if (game !== null) {
+            
+            saveUsersBets(userId);
+            res.redirect("/")
+          } else {
 
-
-
-  games.findOne({round})
-  .then(game => {
-    console.log(game)
-    if (game.round === round) {
-      res.render('/', { message: 'The game already exists' });
-      console.log("existe")
-      return;
-    }
-    // else{
-    //   console.log("NO EXISTE :)")
-    // }
-    const newGame = new games({
-    round,
-    match1,
-    match2,
-    match3,
-    match4,
-    match5,
-    match6,
-    match7,
-    match8,
-    match9,
-    match10,
-    results,
-    users
-  })
-  newGame.save()
-    .then((games) => {
-      res.redirect('/');
-    })
-    .catch((error) => {
-      console.log('Error while creating new game');
-      res.render("/");
-    })
-
-    
-  })
+            const newGame = new games({
+              round,
+              match1,
+              match2,
+              match3,
+              match4,
+              match5,
+              match6,
+              match7,
+              match8,
+              match9,
+              match10,
+              results,
+              users
+            })
+            newGame.save()
+              .then((games) => {
+                res.redirect('/');
+              })
+              .catch((error) => {
+                console.log('Error while creating new game');
+                res.render("/");
+              })
+            }
+            saveUsersBets(userId);
+        })
+        // .catch(error => {
+        //   console.log('Error trying to edit the movie: ', error);
+        // })
+})
 
 
-
-  
-});
-
-module.exports = router;
+      module.exports = router;
